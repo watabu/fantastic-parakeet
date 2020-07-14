ba9d156bf2e91e6b2f28f012d99caa317edf2420
watabu <45591083+watabu@users.noreply.github.com>
2020-07-14 15:23:58 +0900

    add clojure
    clojureを追加

---
+++ 0714_stdio/Clojure.clj
@@ -0,0 +1,7 @@
+;Clojure watabu
+(print "“ü—ÍF")
+(flush)
+(def line (read-line))
+(newline)
+(print (str "“ü—Í‚µ‚½•¶š—ñF" line))
+(newline)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fantastic-parakeet
 はじめてのぎっとはぶ
+情報技術クラブ　github講習会用のリポジトリです。
+
+
