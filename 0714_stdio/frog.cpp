b44de28d8292ed1237eeeea7279d2a312458d96d
OldFrog-0 <4619082@ed.tus.ac.jp>
2020-07-14 19:01:38 +0900

    Create frog.cpp
    カエル追加

---
+++ 0714_stdio/frog.cpp
@@ -0,0 +1,15 @@
+//C++ ƒJƒGƒ‹
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string help;
+
+	cout << "‚È‚ñ‚©‚¢‚ê‚Ä" << endl;
+	cin >> help;
+	cout << help << endl;
+
+	return 0;
+}
