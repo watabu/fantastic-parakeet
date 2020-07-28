4cbb97620af45da31b5f7f2146451c382f5c3449
watabu <45591083+watabu@users.noreply.github.com>
2020-07-28 18:40:22 +0900

    Add ambush
    入力された値が負のときにアンブッシュされるように

--- 0728/hello_origin.cpp
+++ 0728/hello_origin.cpp
@@ -10,11 +10,31 @@ void hello_N(int num) {
 	cout << endl;
 }
 
+//敵にアンブッシュされる　値が-10以下なら回避成功
+void ambush(int num) {
+	cout << "<<敵のアンブッシュ！>>\n";
+	if (num <= -10) {
+		cout << "アンブッシュに失敗したニンジャはすれ違い様にカラテを喰らい爆発四散！\n";
+	}
+	else {
+		cout << "アンブッシュも見抜けないサンシタニンジャは爆発四散！\n";
+	}
+
+}
+
 int main() {
 	int N;
+	cout << "何回あいさつする？:";
 	cin >> N;
-	
+
+	if (N <= 0) {
+		ambush(N);
+		return 0;
+	}
+
 	hello_N(N);
+
+	cout << "挨拶終わり\n";
 
 	return 0;
 }
