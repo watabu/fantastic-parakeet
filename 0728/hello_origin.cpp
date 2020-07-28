9c112fd42314bd798e717740e49c03b247508bb7
watabu <45591083+watabu@users.noreply.github.com>
2020-07-28 18:17:37 +0900

    Add hello.cpp
    挨拶をするソースコードを追加

---
+++ 0728/hello_origin.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < num; i++) {
+		cout << "hello ";
+	}
+	cout << endl;
+
+	return 0;
+}
