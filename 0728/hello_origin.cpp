0f76abb8f629cf878a765866ad7dbf78b2044f10
watabu <45591083+watabu@users.noreply.github.com>
2020-07-28 18:24:26 +0900

    Add hello_N
    挨拶をする関数を追加

--- 0728/hello_origin.cpp
+++ 0728/hello_origin.cpp
@@ -2,15 +2,19 @@
 #include <iostream>
 using namespace std;
 
+//®” num ‰ñ‚ ‚¢‚³‚Â‚ğ‚·‚é
+void hello_N(int num) {
+	for (int i = 0; i < num; i++) {
+		cout << "hello ";
+	}
+	cout << endl;
+}
 
 int main() {
 	int N;
 	cin >> N;
 	
-	for (int i = 0; i < num; i++) {
-		cout << "hello ";
-	}
-	cout << endl;
+	hello_N(N);
 
 	return 0;
 }
